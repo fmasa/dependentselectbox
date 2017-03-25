0cfce7f520ad6960609d6d372b467471054a1f3b
František Maša <frantisekmasa1@gmail.com>
2017-03-25 17:47:27 +0100

    Changed coding standard.

--- src/DependentSelectBox/DependentSelectBox.php
+++ src/DependentSelectBox/DependentSelectBox.php
@@ -14,7 +14,8 @@ use Nette\NotSupportedException;
 use Nette\Forms\Container as FormContainer;
 
 
-class DependentSelectBox extends SelectBox {
+class DependentSelectBox extends SelectBox
+{
 
 // <editor-fold defaultstate="collapsed" desc="variables">
 
@@ -58,19 +59,20 @@ class DependentSelectBox extends SelectBox {
 	 * @param SelectBox|DependentSelectBox|FormControlDependencyHelper $depends SelectBox on which is this control attached
 	 * @param callback $dataCallback Callback for data retreiving function($form, $dependentSelectBox)
 	 */
-	public function __construct($label, $parents, callable $dataCallback) {
+	public function __construct($label, $parents, callable $dataCallback)
+	{
 		parent::__construct($label, null, null);
 		$this->dataCallback = $dataCallback;
-		if(!is_array($parents))
+		if (!is_array($parents))
 			$parents = ($parents === null) ? array() : array($parents);
 		$this->parents = $parents;
-		foreach($parents as $parent) {
+		foreach ($parents as $parent) {
 			$dependencyHelper = $this->createDependencyHelper($parent);
 			$dependencyHelper->addOnChangeCallback(array($this, "submitButtonHandler"));
 
-			if($parent instanceof DependentSelectBox)
+			if ($parent instanceof DependentSelectBox)
 				$parent->childs[] = $this;
-			if(!self::$disableChilds && $this->isRoot($parent) && $parent instanceof SelectBox && $parent->getPrompt())
+			if (!self::$disableChilds && $this->isRoot($parent) && $parent instanceof SelectBox && $parent->getPrompt())
 				throw new NotSupportedException('When first item on root is skipped, $disableChilds = false cant be used !');
 		}
 	}
@@ -80,7 +82,8 @@ class DependentSelectBox extends SelectBox {
 	 * @param  IComponent
 	 * @return void
 	 */
-	protected function attached($form) {
+	protected function attached($form)
+	{
 		parent::attached($form);
 		$this->processAttaching($form);
 	}
@@ -89,7 +92,8 @@ class DependentSelectBox extends SelectBox {
 	 * Process all attaching methods
 	 * @param Form $form
 	 */
-	protected function processAttaching($form) {
+	protected function processAttaching($form)
+	{
 		$this->autoSelectRootFirstItem();
 		$this->initializeState($form);
 	}
@@ -98,13 +102,14 @@ class DependentSelectBox extends SelectBox {
 	 * Initialize state of control. "Disable" control or set value ...
 	 * @param Form $form
 	 */
-	protected function initializeState($form) {
-		if(self::$disableChilds && $this->hasAnyParentEmptyValue()) {
+	protected function initializeState($form)
+	{
+		if (self::$disableChilds && $this->hasAnyParentEmptyValue()) {
 			$this->disableSelectBox();
 		} else {
 			$this->isDisabled = false;
 			$this->setItemsFromCallback($form);
-			if(self::$disableChilds && ($this->hasEmptyValue() || $this->leaveFirstEmpty)) {
+			if (self::$disableChilds && ($this->hasEmptyValue() || $this->leaveFirstEmpty)) {
 				$this->addEmptyHeaderItem($this->hasEmptyValue());
 			}
 		}
@@ -119,12 +124,13 @@ class DependentSelectBox extends SelectBox {
 	 * @param mixed $value
 	 * @param boolean $refreshTree Remake tree of components? (false only for internal system)
 	 */
-	public function setValue($value, $refreshTree = true) {
-		if($value !== null && !$this->hasEmptyValue() && $this->hasAnyParentEmptyValue()) {
+	public function setValue($value, $refreshTree = true)
+	{
+		if ($value !== null && !$this->hasEmptyValue() && $this->hasAnyParentEmptyValue()) {
 			throw new LogicException("Cant set value of dependent component when parent have no value ! (Control: '$this->name')");
 		}
 		parent::setValue($value);
-		if($refreshTree && !$this->hasEmptyValue())
+		if ($refreshTree && !$this->hasEmptyValue())
 			$this->refresh(false);
 		return $this;
 	}
@@ -134,9 +140,10 @@ class DependentSelectBox extends SelectBox {
 	 * @param array array($key => $value)
 	 * @return DependentSelectBox
 	 */
-	public function setDisabledValue($disabledValue = null) {
+	public function setDisabledValue($disabledValue = null)
+	{
 		$this->disabledValue = $disabledValue;
-		if($this->isDependentSelectBoxDisabled())
+		if ($this->isDependentSelectBoxDisabled())
 			$this->disableSelectBox();
 		return $this;
 	}
@@ -145,8 +152,9 @@ class DependentSelectBox extends SelectBox {
 	 * Sets default value for control and remake subtree of components
 	 * @param mixed
 	 */
-	public function setDefaultValue($value) {
-		if(!$this->hasEmptyValue() && $this->hasAnyParentEmptyValue()) {
+	public function setDefaultValue($value)
+	{
+		if (!$this->hasEmptyValue() && $this->hasAnyParentEmptyValue()) {
 			throw new LogicException("Cant set value of dependent component when parent have no value ! (Control: '$this->name')");
 		}
 		parent::setDefaultValue($value);
@@ -159,8 +167,9 @@ class DependentSelectBox extends SelectBox {
 	 * @param  array
 	 * @return DependentSelectBox  provides a fluent interface
 	 */
-	public function setItems(array $items, $useKeys = TRUE) {
-		if(!$useKeys)
+	public function setItems(array $items, $useKeys = TRUE)
+	{
+		if (!$useKeys)
 			throw new NotSupportedException("Working without keys not supported !");
 		return parent::setItems($items, $useKeys);
 	}
@@ -171,14 +180,16 @@ class DependentSelectBox extends SelectBox {
 	 * @param mixed Parameters passed to callback
 	 * @return DependentSelectBox  provides a fluent interface
 	 */
-	public function addOnSubmitCallback(callable $callback, $parameter = null, $_ = null) {
+	public function addOnSubmitCallback(callable $callback, $parameter = null, $_ = null)
+	{
 		$params = func_get_args();
 		unset($params[0]);
 		$this->onSubmit[] = array($callback, $params);
 		return $this;
 	}
 
-	public function setLeaveFirstEmpty($leaveFirstEmpty) {
+	public function setLeaveFirstEmpty($leaveFirstEmpty)
+	{
 		$this->leaveFirstEmpty = $leaveFirstEmpty;
 		$this->refresh(false, false);
 	}
@@ -186,7 +197,8 @@ class DependentSelectBox extends SelectBox {
 	/**
 	 * @return SelectBox[]|DependentSelectBox[]
 	 */
-	public function getDependentParents() {
+	public function getDependentParents()
+	{
 		return $this->parents;
 	}
 
@@ -199,18 +211,19 @@ class DependentSelectBox extends SelectBox {
 	 * Handler which is called when form is submmited by button of this control
 	 * @param SubmitButton $button
 	 */
-	public function submitButtonHandler($button) {
+	public function submitButtonHandler($button)
+	{
 		$form = $button->getForm();
 
-		foreach($this->onSubmit as $onSubmitItem) {
+		foreach ($this->onSubmit as $onSubmitItem) {
 			list($callback, $params) = $onSubmitItem;
 			$callback->invokeArgs($params);
 		}
 
-		if($this->hasAnyParentEmptyValue())
+		if ($this->hasAnyParentEmptyValue())
 			return;
 
-		if(self::$disableChilds) {
+		if (self::$disableChilds) {
 			$this->addEmptyHeaderItem();
 			$this->disableAllChilds();
 		} else {
@@ -229,13 +242,14 @@ class DependentSelectBox extends SelectBox {
 	 * @param boolean $clearValue
 	 * @param boolean $recursive Refresh all items in tree or only this item ?
 	 */
-	public function refresh($clearValue = false, $recursive = true) {
+	public function refresh($clearValue = false, $recursive = true)
+	{
 		$this->setPrompt(false);
-		if($clearValue)
+		if ($clearValue)
 			$this->setValue(null, false);
 		$this->initializeState($this->getForm());
-		if($recursive) {
-			foreach($this->childs as $child) {
+		if ($recursive) {
+			foreach ($this->childs as $child) {
 				$child->refresh();
 			}
 		}
@@ -247,8 +261,9 @@ class DependentSelectBox extends SelectBox {
 	 * @param FormControl $control
 	 * @return FormControlDependencyHelper
 	 */
-	protected function createDependencyHelper($control) {
-		if($control instanceof FormControlDependencyHelper)
+	protected function createDependencyHelper($control)
+	{
+		if ($control instanceof FormControlDependencyHelper)
 			return $control;
 		$fcdh = new FormControlDependencyHelper($control);
 		$fcdh->setButtonText("Load");
@@ -258,14 +273,15 @@ class DependentSelectBox extends SelectBox {
 	/**
 	 * Test whenever $this->autoSelectRootFirstItem is true and if parent is root SelectBox, select first item
 	 */
-	protected function autoSelectRootFirstItem() {
-		if($this->autoSelectRootFirstItem) {
-			foreach($this->parents as $parent) {
-				if($this->isRoot($parent) && $parent->getValue() === null) {
-					if(!($parent instanceof SelectBox))
+	protected function autoSelectRootFirstItem()
+	{
+		if ($this->autoSelectRootFirstItem) {
+			foreach ($this->parents as $parent) {
+				if ($this->isRoot($parent) && $parent->getValue() === null) {
+					if (!($parent instanceof SelectBox))
 						throw new InvalidArgumentException("When using 'autoSelectRootFirstItem = true', parent must be instance of SelectBox !");
 					$items = $parent->getItems();
-					if(!empty($items)) {
+					if (!empty($items)) {
 						$items = array_keys($items);
 						$parent->setValue(reset($items));
 					}
@@ -278,13 +294,14 @@ class DependentSelectBox extends SelectBox {
 	 * Tests if any parent control have empty value
 	 * @return booleam
 	 */
-	protected function hasAnyParentEmptyValue() {
-		foreach($this->parents as $parent) {
-			if($parent instanceof DependentSelectBox) {
-				if($parent->hasEmptyValue())
+	protected function hasAnyParentEmptyValue()
+	{
+		foreach ($this->parents as $parent) {
+			if ($parent instanceof DependentSelectBox) {
+				if ($parent->hasEmptyValue())
 					return true;
 			} else {
-				if(in_array($parent->getValue(), self::$emptyValues, true))
+				if (in_array($parent->getValue(), self::$emptyValues, true))
 					return true;
 			}
 		}
@@ -295,7 +312,8 @@ class DependentSelectBox extends SelectBox {
 	 * Tests if this control have empty value
 	 * @return boolean
 	 */
-	protected function hasEmptyValue() {
+	protected function hasEmptyValue()
+	{
 		return $this->isDependentSelectBoxDisabled() || !$this->isSelected();
 	}
 
@@ -304,13 +322,14 @@ class DependentSelectBox extends SelectBox {
 	 * Load items from callback
 	 * @param Form $form Form with values
 	 */
-	protected function setItemsFromCallback($form) {
+	protected function setItemsFromCallback($form)
+	{
 		$callback = $this->dataCallback;
 		$data = $callback($form, $this);
-		if(!is_array($data))
+		if (!is_array($data))
 			throw new InvalidArgumentException("Data must be array !");
 		$this->setItems($data);
-		if(!self::$disableChilds && $this->hasEmptyValue()) {
+		if (!self::$disableChilds && $this->hasEmptyValue()) {
 			$this->setFirstItemSelected();
 		}
 	}
@@ -318,8 +337,9 @@ class DependentSelectBox extends SelectBox {
 	/**
 	 * "Disable" all childs DependentSelectBox which are attached
 	 */
-	protected function disableAllChilds() {
-		foreach($this->childs as $child) {
+	protected function disableAllChilds()
+	{
+		foreach ($this->childs as $child) {
 			$child->disableSelectBox();
 			$child->disableAllChilds();
 		}
@@ -328,8 +348,9 @@ class DependentSelectBox extends SelectBox {
 	/**
 	 * "Disable" all childs DependentSelectBox which are attached
 	 */
-	protected function selectFirstItemForAllChilds() {
-		foreach($this->childs as $child) {
+	protected function selectFirstItemForAllChilds()
+	{
+		foreach ($this->childs as $child) {
 			$child->setFirstItemSelected();
 			$child->refresh(false, false);
 			$child->selectFirstItemForAllChilds();
@@ -339,12 +360,13 @@ class DependentSelectBox extends SelectBox {
 	/**
 	 * "Disable" this control
 	 */
-	protected function disableSelectBox() {
+	protected function disableSelectBox()
+	{
 		$this->isDisabled = true;
-		if($this->getControlPrototype()->class == null)
+		if ($this->getControlPrototype()->class == null)
 			$this->getControlPrototype()->class = "";
 		$this->getControlPrototype()->addClass(self::$disabledHtmlClass);
-		if($this->disabledValue === null) {
+		if ($this->disabledValue === null) {
 			$this->setValue(null, false);
 			$this->setItems(array());
 			$this->setPrompt(self::$disabledItemTitle);
@@ -356,11 +378,13 @@ class DependentSelectBox extends SelectBox {
 		}
 	}
 
-	protected function isSelected() {
+	protected function isSelected()
+	{
 		return !$this->isDependentSelectBoxDisabled() && !in_array($this->getValue(), self::$emptyValues, true);
 	}
 
-	protected function isDependentSelectBoxDisabled() {
+	protected function isDependentSelectBoxDisabled()
+	{
 		return $this->isDisabled;
 	}
 
@@ -369,33 +393,38 @@ class DependentSelectBox extends SelectBox {
 	 * @param Control $control item to check
 	 * @return boolean
 	 */
-	protected function isRoot($control) {
+	protected function isRoot($control)
+	{
 		return !($control instanceof DependentSelectBox);
 	}
 
 	/**
 	 * Add empty header item and select him
 	 */
-	protected function addEmptyHeaderItem($selectFirst = true) {
+	protected function addEmptyHeaderItem($selectFirst = true)
+	{
 		$this->setPrompt(self::$emptyValueTitle);
-		if($selectFirst)
+		if ($selectFirst)
 			$this->setFirstItemSelected();
 	}
 
 	/**
 	 * Select first item
 	 */
-	protected function setFirstItemSelected() {
+	protected function setFirstItemSelected()
+	{
 		$keys = array_keys($this->getItems());
 		$key = reset($keys);
 		$this->setValue($key, false);
 	}
 
-	public static function Container_prototype_addDependentSelectBox(FormContainer $obj, $name, $label, $parents, $dataCallback) {
+	public static function Container_prototype_addDependentSelectBox(FormContainer $obj, $name, $label, $parents, $dataCallback)
+	{
 		return $obj[$name] = new DependentSelectBox($label, $parents, $dataCallback);
 	}
 
-	public static function register($methodName = "addDependentSelectBox") {
+	public static function register($methodName = "addDependentSelectBox")
+	{
 		FormContainer::extensionMethod($methodName, "DependentSelectBox\DependentSelectBox::Container_prototype_addDependentSelectBox");
 	}
 
--- src/DependentSelectBox/JsonDependentSelectBox.php
+++ src/DependentSelectBox/JsonDependentSelectBox.php
@@ -19,28 +19,31 @@ class JsonDependentSelectBox extends DependentSelectBox
 
 	public static $jsonResoponseItems = array();
 
-	public function submitButtonHandler($button) {
+	public function submitButtonHandler($button)
+	{
 		parent::submitButtonHandler($button);
 		if ($this->lookup("\Nette\Application\UI\Presenter")->isAjax())
 			$this->addJsonResponseItem($this);
 	}
 
-	protected function addJsonResponseItem($selectBox) {
+	protected function addJsonResponseItem($selectBox)
+	{
 		self::$jsonResoponseItems[] = $selectBox;
-		if($selectBox instanceof DependentSelectBox)
-			foreach($selectBox->childs as $child)
+		if ($selectBox instanceof DependentSelectBox)
+			foreach ($selectBox->childs as $child)
 				$child->addJsonResponseItem($child);
 	}
 
-	public static function tryJsonResponse(Presenter $presenter) {
-		if(empty(self::$jsonResoponseItems))
+	public static function tryJsonResponse(Presenter $presenter)
+	{
+		if (empty(self::$jsonResoponseItems))
 			return;
-		
+
 		$payload = array(
 			"type" => "JsonDependentSelectBoxResponse",
 			"items" => array()
 		);
-		foreach(self::$jsonResoponseItems as $item) {
+		foreach (self::$jsonResoponseItems as $item) {
 			$payload["items"][$item->getHtmlId()] = array(
 				"selected" => $item->getValue(),
 				"items" => $item->getItems()
@@ -54,16 +57,19 @@ class JsonDependentSelectBox extends DependentSelectBox
 	/**
 	 * @deprecated Alias for Container_prototype_addDependentSelectBox
 	 */
-	public static function formAddJsonDependentSelectBox($_this, $name, $label, $parents, $dataCallback) {
+	public static function formAddJsonDependentSelectBox($_this, $name, $label, $parents, $dataCallback)
+	{
 		return self::Container_prototype_addJsonDependentSelectBox($_this, $name, $label, $parents, $dataCallback);
 	}
 
-	public static function Container_prototype_addJsonDependentSelectBox(FormContainer $obj, $name, $label, $parents, $dataCallback) {
+	public static function Container_prototype_addJsonDependentSelectBox(FormContainer $obj, $name, $label, $parents, $dataCallback)
+	{
 		return $obj[$name] = new JsonDependentSelectBox($label, $parents, $dataCallback);
 	}
 
-	public static function register($methodName = "addJsonDependentSelectBox") {
+	public static function register($methodName = "addJsonDependentSelectBox")
+	{
 		FormContainer::extensionMethod($methodName, "DependentSelectBox\JsonDependentSelectBox::Container_prototype_addJsonDependentSelectBox");
 	}
-	
+
 }
